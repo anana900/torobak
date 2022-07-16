cbf87c19091d14833f9d427dfce017bcc170d006
anana <anana900@wp.pl>
2022-07-17 01:15:54 +0200

    Glowica mini idea.

--- komponenty/odleglosc_pomiar/vl53l0x.py
+++ komponenty/odleglosc_pomiar/vl53l0x.py
@@ -3,11 +3,21 @@ import busio
 import adafruit_vl53l0x
 from time import sleep
 
-i2c  = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_vl53l0x.VL53L0X(i2c)
-# sensor.measurement_timing_budget = 33000
 
-while True:
-    print(f"{sensor.range}")
-    sleep(0.1)
+class PomiarOdleglosci:
+    def __init__(self):
+        self.i2c  = busio.I2C(board.SCL, board.SDA)
+        self.sensor = adafruit_vl53l0x.VL53L0X(self.i2c)
+        # sensor.measurement_timing_budget = 33000
 
+    def pomiar_odleglosci_mm(self):
+        return self.sensor.range
+
+def test_pomiar():
+    po = PomiarOdleglosci()
+    while True:
+        print(f"{po.pomiar_odleglosci_mm()}")
+        sleep(0.1)
+
+if __name__ == '__main__':
+    test_pomiar()
