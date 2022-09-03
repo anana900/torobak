e685ffcc063b82035ebb4575cd01f8c9a4e8ce62
anana <anana900@wp.pl>
2022-09-04 01:11:22 +0200

    Silnik PWM + Hall szukanie rozwiązania problemów z liczbą pulsów.

--- komponenty/silniki_sterowanie_PWM/silnik_pwm_hall_v3.py
+++ komponenty/silniki_sterowanie_PWM/silnik_pwm_hall_v3.py
@@ -82,7 +82,8 @@ pca = ServoKit(channels = serwo_ilosc_kanalow)
 
 # Konfiguracja do czujnika HALL
 HALL_BCM = 21
-#GPIO.setup(HALL_BCM, GPIO.IN)
+# GPIO.setup(HALL_BCM, GPIO.IN)
+# Internal pull-up - zewnętrzy rezystor odłączony
 GPIO.setup(HALL_BCM, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Event do zatrzymywania pracy watkwo w przypadku ctrl+c
@@ -92,11 +93,10 @@ event_stop = Event()
 serwo_ilosc_kanalow = 16
 pca = ServoKit(channels = serwo_ilosc_kanalow)
 
-def czujnik_hall(zadane_zbocza_lock) -> None:
-    """
-    Zlicza zbocza z czujnika halla.
-    """
-    print("Watek hall start")
+_FULL_ROUND_PULSES_BROSE_1 = 260   # 12V BROSE BRG-1101-965575-100
+
+def czujnik_hall_v1(zadane_zbocza_lock: Lock) -> None:
+    print("Watek hall v1 start")
     global zadane_zbocza
     hall_tick_counter = 0
     hall_status = GPIO.input(HALL_BCM)
@@ -110,11 +110,45 @@ def czujnik_hall(zadane_zbocza_lock) -> None:
             with zadane_zbocza_lock:
                 zadane_zbocza -= 1
     print(f"zadane zbocza {zadane_zbocza} hall_counter {hall_tick_counter}")
-    print("Watek hall stop")
+    print("Watek hall v1 stop")
+
+def czujnik_hall_v2(zadane_zbocza_lock: Lock) -> None:
+    print("Watek hall v2 start")
+    global zadane_zbocza
+    licznik = 0
+    while True:
+        if event_stop.is_set():
+            break
+        GPIO.wait_for_edge(HALL_BCM, GPIO.BOTH)
+        licznik += 1
+        with zadane_zbocza_lock:
+            zadane_zbocza -= 1
+    print(f"Zliczone impulsy {licznik}")
+    print("Watek hall v2 stop")
+
+def czujnik_hall_v2x(zadane_zbocza_lock: Lock) -> None:
+    print("Watek hall v3 start")
+    global zadane_zbocza
+    licznik = 0
+    GPIO.add_event_detect(HALL_BCM, GPIO.BOTH)
+    while True:
+        if GPIO.event_detected(HALL_BCM):
+            licznik += 1
+            zadane_zbocza -= 1
+        if event_stop.is_set():
+            break
+    GPIO.remove_event_detect(HALL_BCM)
+    print(f"Zliczone impulsy {licznik}")
+    print("Watek hall v3 stop")
 
 def przyspieszenie(xlimit: int, x: int) -> float:
+    """
+    acc:   0   0.1  0.2  0.3  0.5  0.7     1.0   0.7   0.5  0.3  0.2  0.1   0
+              a   b    c    d    e    f         f    e    d    c    b     a
+    x:        5   10   15   20   25   30   35   30   25   20   15   10    5
+          ----.----.----.----.----.----.----.----.----.----.----.----.----.----xlimit
+    """
     a, b, c, d, e, f = 5, 10, 15, 20, 25, 30
-    acc_table = {0: 5, 0.1: 10, 0.2: 15, 0.3: 20, 0.5: 25, 0.7: 30}
     acc = -1
     if x <= a or xlimit-x <= a:
         acc = 0
@@ -131,10 +165,10 @@ def przyspieszenie(xlimit: int, x: int) -> float:
     elif x > f:
         acc = 1
     else:
-        raise(f"This should not happen. Acc cannot be calculated. x: {x} xlimit: {xlimit}")
+        raise(f"To nie powinno się wydażyć! Acc nie może zostać ustawione. x: {x} xlimit: {xlimit}")
     return acc
 
-def silnik_pwm(port_lewo: int, port_prawo: int, kierunek, zadane_zbocza_lock) -> None:
+def silnik_pwm(port_lewo: int, port_prawo: int, kierunek, zadane_zbocza_lock: Lock) -> None:
     print("Watek silnik start")
     global zadane_zbocza
     min_szerokosc_pulsu_us = 0
@@ -163,43 +197,48 @@ def silnik_pwm(port_lewo: int, port_prawo: int, kierunek, zadane_zbocza_lock) ->
     print(f"zadane zbocza {zadane_zbocza}")
     print("Watek silnik stop")
 
-_ile = 10
+_ile = 0
 zadane_zbocza = _ile
-def silnik_sterowanie() -> None:
+def dc_motor_control(angle, direction) -> None:
     global zadane_zbocza
-    kierunek = 0
     zadane_zbocza_lock = Lock()
+    zadane_zbocza = int(_FULL_ROUND_PULSES_BROSE_1 * angle / 360)
     try:
-        while True:
-            kierunek = 0 if kierunek else 1
-            watek_silnik_czujnik_hall = Thread(target=czujnik_hall, args=(zadane_zbocza_lock,))
-            watek_silnik_pwm = Thread(target=silnik_pwm, args=(14, 15, kierunek, zadane_zbocza_lock,))
-            #watek_zliczanie = Thread(target=zliczanie, args=(zadane_zbocza_lock,))
-            watek_silnik_czujnik_hall.start()
-            watek_silnik_pwm.start()
-            #watek_zliczanie.start()
-            watek_silnik_czujnik_hall.join()
-            watek_silnik_pwm.join()
-            #watek_zliczanie.join()
-            time.sleep(1)
-            event_stop.clear()
-            zadane_zbocza = _ile
+        watek_silnik_czujnik_hall_v1 = Thread(target=czujnik_hall_v1, args=(zadane_zbocza_lock,))
+        #watek_silnik_czujnik_hall_v2 = Thread(target=czujnik_hall_v2, args=(zadane_zbocza_lock,))
+        watek_silnik_pwm = Thread(target=silnik_pwm, args=(14, 15, direction, zadane_zbocza_lock,))
+        watek_silnik_czujnik_hall_v1.start()
+        #watek_silnik_czujnik_hall_v2.start()
+        watek_silnik_pwm.start()
+        watek_silnik_czujnik_hall_v1.join()
+        #watek_silnik_czujnik_hall_v2.join()
+        watek_silnik_pwm.join()
+        event_stop.clear()
     except KeyboardInterrupt:
         print("Zakonczenie poprzez ctrl+c")
         event_stop.set()
     except Exception as unknown_exception:
         print(f"To nie powinno sie wydarzyc! {unknown_exception:}")
     finally:
-        print("Czyszczenie GPIO!")
         pca_cleanup(14, 15)
-        GPIO.cleanup()
 
 def pca_cleanup(port_lewo: int, port_prawo: int) -> None:
     """
-    Czyszczenie
+    Czyszczenie sterownika PWM
     """
+    print(f"Zerowanie sterowanika PWM na portach: {port_lewo} {port_prawo}")
     pca.continuous_servo[port_prawo].throttle = -1
     pca.continuous_servo[port_lewo].throttle = -1
 
+def gpio_cleanip() -> None:
+    """
+    Czyszczenie portów GPIO
+    """
+    GPIO.cleanup()
+
 if __name__=='__main__':
-    silnik_sterowanie()
+    direction = 1
+    for angle in range(0, 361, 30):
+        direction = 0 if direction else 1
+        dc_motor_control(angle, direction)
+    gpio_cleanip()
